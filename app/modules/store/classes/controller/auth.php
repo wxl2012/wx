bedf57fc792072e6d8e1d32f764cc1dd1ccb217a
rayby2013 <zyr.wxl@gmail.com>
2016-04-27 23:16:38 +0800

    添加店铺认证功能 添加提现申请 提现申请记录查看

---
+++ app/modules/store/classes/controller/auth.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 默认控制器
+ *
+ * @package    app
+ * @version    1.0
+ * @author     Ray 33705910@qq.com
+ * @license    MIT License
+ * @copyright  2013 - 2015 Ray
+ * @link       http://wangxiaolei.cn
+ */
+
+namespace store;
+
+class Controller_Auth extends Controller_BaseController {
+
+    public function before(){
+        parent::before();
+    }
+
+    public function action_index(){
+        $this->template->content = \View::forge("{$this->theme}/auth/index");
+    }
+}
---
+++ app/modules/store/views/default/auth/index.php
@@ -0,0 +1,291 @@
+<style type="text/css">
+    .navbar-blue{
+        background-color: #337ab7;
+        color: #fff;
+    }
+    .navbar-blue a{
+        color: #fff;
+    }
+</style>
+<nav class="navbar navbar-blue navbar-fixed-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3" style="line-height: 50px;">
+                <a href="javascript:history.back(-1);"><i class="fa fa-angle-left" style="font-size: 1.5em;"></i></a>
+            </div>
+            <div class="col-xs-6 text-center" style="line-height: 50px;">
+                店铺认证
+            </div>
+            <div class="col-xs-3 text-right" style="line-height: 50px;">
+                <a class="hide">
+                    <i class="fa fa-check" style="font-size: 1.5em;"></i>
+                </a>
+                <a>
+                    <i class="fa fa-edit" style="font-size: 1.5em;"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<div class="container" style="margin-top: 55px;">
+    <div class="row">
+        <div class="col-xs-12">
+            <!-- 企业信息 -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    企业信息
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            企业全称
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            法人代表
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            工商执照注册号
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            组织机构代码证
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            企业规模
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="选填" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            经营范围(一般经营范围)
+                            <textarea name="12" id="12" class="form-control" placeholder="选填"></textarea>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            经营范围(前置许可经营范围)
+                            <textarea name="12" id="12" class="form-control" placeholder="选填"></textarea>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <!-- 法人信息 -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    法人信息
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            真实姓名
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            身份证号
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <!-- 联系方式 -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    联系信息
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            所在地
+                        </div>
+                        <div class="col-xs-8">
+                            <select class="form-control" style="width: 48%; display: inline;">
+                                <option>省份</option>
+                            </select>
+                            <select class="form-control" style="width: 48%; display: inline;">
+                                <option>城市</option>
+                            </select>
+                            <select class="form-control" style="margin-top: 3px;">
+                                <option>区县</option>
+                            </select>
+                            <input type="text" value="" class="form-control" placeholder="街道" style="margin-top: 3px;"/>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            公司网站
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="选填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            手机号
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            电子邮箱
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="必填项" />
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <!-- 银行信息 -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    银行信息
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            开户名称
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="企业开户名称(必填项)" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            开户银行
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="企业开户银行(必填项)" />
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            银行账号
+                        </div>
+                        <div class="col-xs-8">
+                            <input type="text" value="" class="form-control" placeholder="企业银行账号(必填项)" />
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <!-- 认证资料信息 -->
+            <ul class="list-group">
+                <li class="list-group-item">
+                    电子资料
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            营业执照
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            组织机构代码证
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            身份证(正面)
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            身份证(反面)
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            手持身份证照片
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5">
+                            银行卡照片
+                        </div>
+                        <div class="col-xs-7">
+                            <a href="javascript:;">点击上传</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
