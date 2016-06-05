f7d861b3e791be0b61c784c007e97154a959489f
mengxuexue1314 <651463679@qq.com>
2016-06-06 01:58:46 +0800

    订单列表页，无数据时的显示

--- app/modules/restaurant/views/default/order/index.php
+++ app/modules/restaurant/views/default/order/index.php
@@ -1,1 +1,4 @@
-订单列表
+<div class="row" style="color: #aaa;">
+    <div class="col-xs-12 text-center" style="padding-top: 40px; line-height: 50px; font-size: 60pt;"><i class="fa fa-frown-o"></i></div>
+    <div class="col-xs-12 text-center" style="line-height: 50px; font-size: 13pt;">您还未购买过任何东西!</div>
+</div>
